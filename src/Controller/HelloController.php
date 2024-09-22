3b106f28cb5a23fb588ea31dcb0a9b4b279e8f4b
uni51 <unieye51@gmail.com>
2024-09-22 13:25:27 +0900

    2-6 組み込まれているサービスについて確認する

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -23,15 +23,15 @@ class HelloController extends AbstractController
      */
     public function index()
     {
-        $result = <<< EOM
-        <html>
-        <head><title>Hello</title></head>
-        <body>
-        <h1>Hello Symfony!</h1>
-        <p>this is Symfony sample page.</p>
-        </body>
-        </html>
-EOM;
+        $result = '<html><body>';
+        $result .= '<h1>Subscribed Services</h1>';
+        $result .= '<ol>';
+        $arr = $this->getSubscribedServices();
+        foreach ($arr as $key => $value) {
+            $result .= '<li>' . $key . '</li>';
+        }
+        $result.= '</ol>';
+        $result .= '</body></html>';
         return new Response($result);
     }
 }
