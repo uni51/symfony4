e1e2b24b715dffe7b6e1f44a5a6fce3d161faebc
uni51 <unieye51@gmail.com>
2024-09-22 13:39:56 +0900

    2-8 クエリパラメーターの利用

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,11 +3,40 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
+    /**
+     * @Route("/hello", name="hello")
+     */
+    public function index(Request $request)
+    {
+        $name = $request->get('name');
+        $pass = $request->get('pass');
+        $result = '<html><body><ol>';
+        $result .= '<h1>Parameter</h1>';
+        $result .= '<p>name: ' . $name . '</p>';
+        $result .= '<p>pass: ' . $pass . '</p>';
+        $result .= '</body></html>';
+        return new Response($result);
+    }
+
+    /**
+     * @Route("/hello/params/{name}/{pass}", name="params")
+     */
+    public function params($name=('noname'), $pass=('no password')) // ()の中はデフォルト値
+    {
+        $result = '<html><body><ol>';
+        $result .= '<h1>Parameter</h1>';
+        $result .= '<p>name: ' . $name . '</p>';
+        $result .= '<p>pass: ' . $pass . '</p>';
+        $result .= '</body></html>';
+        return new Response($result);
+    }
+
 //    /**
 //     * @Route("/hello", name="app_hello")
 //     */
@@ -18,33 +47,21 @@ class HelloController extends AbstractController
 //        ]);
 //    }
 
-    /**
-     * @Route("/hello", name="hello")
-     */
-    public function index()
-    {
-        $result = '<html><body>';
-        $result .= '<h1>Subscribed Services</h1>';
-        $result .= '<ol>';
-        $arr = $this->getSubscribedServices();
-        foreach ($arr as $key => $value) {
-            $result .= '<li>' . $key . '</li>';
-        }
-        $result.= '</ol>';
-        $result .= '</body></html>';
-        return new Response($result);
-    }
+//    /**
+//     * @Route("/hello", name="hello")
+//     */
+//    public function index()
+//    {
+//        $result = '<html><body>';
+//        $result .= '<h1>Subscribed Services</h1>';
+//        $result .= '<ol>';
+//        $arr = $this->getSubscribedServices();
+//        foreach ($arr as $key => $value) {
+//            $result .= '<li>' . $key . '</li>';
+//        }
+//        $result.= '</ol>';
+//        $result .= '</body></html>';
+//        return new Response($result);
+//    }
 
-    /**
-     * @Route("/hello/params/{name}/{pass}", name="params")
-     */
-    public function params($name=('noname'), $pass=('no password'))
-    {
-        $result = '<html><body><ol>';
-        $result .= '<h1>Parameter</h1>';
-        $result .= '<p>name: ' . $name . '</p>';
-        $result .= '<p>pass: ' . $pass . '</p>';
-        $result .= '</body></html>';
-        return new Response($result);
-    }
 }
