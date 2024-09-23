95ee40170a230a7152607547c18a322cd8670f59
uni51 <unieye51@gmail.com>
2024-09-23 16:01:54 +0900

    3-10. Symfony Forms を利用する

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,6 +17,30 @@ use Symfony\Component\Serializer\Serializer;
 
 class WelcomeController extends AbstractController
 {
+    /**
+     * @Route("/formbuilder", name="welcome.formbuilder")
+     */
+    public function formbuilder(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('name', TextType::class, ['label' => 'Name'])
+            ->add('save', SubmitType::class, ['label' => 'Click'])
+            ->getForm();
+
+
+        if ($request->getMethod() == 'POST'){
+            $form->handleRequest($request);
+            $msg = 'こんにちは、' . $form->get('name')->getData() . 'さん！';
+        } else {
+            $msg = 'お名前をどうぞ！';
+        }
+        return $this->render('welcome/formbuilder.html.twig', [
+            'title' => 'Hello',
+            'message' => $msg,
+            'form' => $form->createView(),
+        ]);
+    }
+
     /**
      * @Route("/form", name="welcome.form")
      */
