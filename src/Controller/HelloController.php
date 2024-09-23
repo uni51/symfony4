882132f3f9bfe7a83d6e06f135be258b75b3ef82
uni51 <unieye51@gmail.com>
2024-09-23 10:25:29 +0900

    3-1. テンプレートエンジン

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -17,16 +18,95 @@ class HelloController extends AbstractController
     /**
      * @Route("/hello", name="hello")
      */
-    public function index(Request $request)
+    public function index()
     {
-        $name = $request->get('name');
-        $pass = $request->get('pass');
-        $result = '<html><body><ol>';
-        $result .= '<h1>Parameter</h1>';
-        $result .= '<p>name: ' . $name . '</p>';
-        $result .= '<p>pass: ' . $pass . '</p>';
-        $result .= '</body></html>';
-        return new Response($result);
+        return $this->render('hello/index.html.twig', [
+            'controller_name' => 'HelloController',
+        ]);
+    }
+
+//    /**
+//     * @Route("/hello", name="hello")
+//     */
+//    public function index(Request $request)
+//    {
+//        $name = $request->get('name');
+//        $pass = $request->get('pass');
+//        $result = '<html><body><ol>';
+//        $result .= '<h1>Parameter</h1>';
+//        $result .= '<p>name: ' . $name . '</p>';
+//        $result .= '<p>pass: ' . $pass . '</p>';
+//        $result .= '</body></html>';
+//        return new Response($result);
+//    }
+
+    /**
+     * @Route("/loginfo", name="loginfo")
+     */
+    public function loginfo(Request $request, LoggerInterface $logger)
+    {
+        $data = array(
+            'name'=>array('first'=>'Taro','second'=>'Yamada'),
+            'age'=>36,
+            'mail'=>'taro@yamada.kun'
+        );
+        $logger->info(serialize($data));
+        return new JsonResponse($data);
+    }
+
+    /**
+     * @Route("/http_ok", name="http_ok")
+     */
+    public function httpok(Request $request)
+    {
+        $content = <<< EOM
+        <html><head><title>Hello</title></head>
+        <body><h1>Hello!</h1>
+        <p>this is Symfony sample page.</p>
+        </body></html>
+EOM;
+        $response = new Response(
+            $content,
+            Response::HTTP_OK,
+            array('content-type' => 'text/html')
+        );
+        return $response;
+    }
+
+    /**
+     * @Route("/notfound", name="notfound")
+     */
+    public function notfound(Request $request)
+    {
+        $content = <<< EOM
+        <html><head><title>ERROR</title></head>
+        <body><h1>ERROR! 404</h1>
+        </body></html>
+EOM;
+        $response = new Response(
+            $content,
+            Response::HTTP_NOT_FOUND,
+            array('content-type' => 'text/html')
+        );
+        return $response;
+    }
+
+    /**
+     * @Route("/error", name="error")
+     */
+    public function error(Request $request)
+    {
+        $content = <<< EOM
+        <html><head><title>ERROR</title></head>
+        <body><h1>ERROR! 500</h1>
+        </body></html>
+EOM;
+        $response = new Response(
+            $content,
+            Response::HTTP_INTERNAL_SERVER_ERROR,
+            array('content-type' => 'text/html')
+        );
+        return $response;
     }
 
     /**
@@ -35,9 +115,9 @@ class HelloController extends AbstractController
     public function jsonmethod(Request $request)
     {
         $data = array(
-            'name'=>array('first'=>'Taro','second'=>'Yamada'),
-            'age'=>36,
-            'mail'=>'taro@yamada.kun'
+            'name' => array('first' => 'Taro', 'second' => 'Yamada'),
+            'age' => 36,
+            'mail' => 'taro@yamada.kun'
         );
         return new JsonResponse($data);
     }
@@ -53,9 +133,9 @@ class HelloController extends AbstractController
 
 
         $data = array(
-            'name'=>array('first'=>'Hanako','second'=>'Tanaka'),
-            'age'=>29,
-            'mail'=>'hanako@flower.san'
+            'name' => array('first' => 'Hanako', 'second' => 'Tanaka'),
+            'age' => 29,
+            'mail' => 'hanako@flower.san'
         );
 
         $response = new Response();
@@ -68,9 +148,9 @@ class HelloController extends AbstractController
     /**
      * @Route("/other/{domain}", name="other")
      */
-    public function other(Request $request, $domain='')
+    public function other(Request $request, $domain = '')
     {
-        if ($domain == ''){
+        if ($domain == '') {
             return $this->redirect('/hello');
         } else {
             return new RedirectResponse("http://{$domain}.com");
@@ -80,7 +160,7 @@ class HelloController extends AbstractController
     /**
      * @Route("/hello/params/{name}/{pass}", name="params")
      */
-    public function params($name=('noname'), $pass=('no password')) // ()の中はデフォルト値
+    public function params($name = ('noname'), $pass = ('no password')) // ()の中はデフォルト値
     {
         $result = '<html><body><ol>';
         $result .= '<h1>Parameter</h1>';
