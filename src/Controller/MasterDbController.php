9baef5777fb59c8058e265851b4cd53b000a4d47
uni51 <unieye51@gmail.com>
2024-09-28 10:18:10 +0900

    5-16. ネイティブSQLの実行

--- src/Controller/MasterDbController.php
+++ src/Controller/MasterDbController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
 use App\Entity\Person;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -27,30 +28,72 @@ class MasterDbController extends AbstractController
             ->add('save', SubmitType::class, array('label' => 'Click'))
             ->getForm();
 
+
         $repository = $this->getDoctrine()
             ->getRepository(Person::class);
 
         $manager = $this->getDoctrine()->getManager();
+        $mapping = new ResultSetMappingBuilder($manager);
+        $mapping->addRootEntityFromClassMetadata('App\Entity\Person', 'p');
 
         if ($request->getMethod() == 'POST') {
             $form->handleRequest($request);
             $findstr = $form->getData()->getFind();
+            $arr = explode(',', $findstr);
 
-            $query = $manager->createQuery(
-                "SELECT p FROM App\Entity\Person p 
-            WHERE p.name = '{$findstr}'");
+            $query = $manager->createNativeQuery(
+                'SELECT * FROM person WHERE age BETWEEN ? AND ?', $mapping)
+                ->setParameters(array(1 => $arr[0], 2 => $arr[1]));
 
             $result = $query->getResult();
         } else {
-            $result = $repository->findAllwithSort();
-        }
+            $query = $manager->createNativeQuery(
+                'SELECT * FROM person', $mapping);
 
+            $result = $query->getResult();
+        }
         return $this->render('masterdb/find.html.twig', [
             'title' => 'Hello',
             'form' => $form->createView(),
             'data' => $result,
         ]);
     }
+
+//    /**
+//     * @Route("/masterdb/find", name="masterdb.find")
+//     */
+//    public function find(Request $request)
+//    {
+//        $formobj = new FindForm();
+//        $form = $this->createFormBuilder($formobj)
+//            ->add('find', TextType::class)
+//            ->add('save', SubmitType::class, array('label' => 'Click'))
+//            ->getForm();
+//
+//        $repository = $this->getDoctrine()
+//            ->getRepository(Person::class);
+//
+//        $manager = $this->getDoctrine()->getManager();
+//
+//        if ($request->getMethod() == 'POST') {
+//            $form->handleRequest($request);
+//            $findstr = $form->getData()->getFind();
+//
+//            $query = $manager->createQuery(
+//                "SELECT p FROM App\Entity\Person p
+//            WHERE p.name = '{$findstr}'");
+//
+//            $result = $query->getResult();
+//        } else {
+//            $result = $repository->findAllwithSort();
+//        }
+//
+//        return $this->render('masterdb/find.html.twig', [
+//            'title' => 'Hello',
+//            'form' => $form->createView(),
+//            'data' => $result,
+//        ]);
+//    }
 
 //    /**
 //     * @Route("/masterdb/find", name="masterdb.find")
