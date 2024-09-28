04c72832127648ff2238d84f1fd113b5a5c3e230
uni51 <unieye51@gmail.com>
2024-09-28 13:42:40 +0900

    5-25. MessageRespitory の修正

--- src/Repository/MessageRepository.php
+++ src/Repository/MessageRepository.php
@@ -13,7 +13,6 @@ use Doctrine\Persistence\ManagerRegistry;
  *
  * @method Message|null find($id, $lockMode = null, $lockVersion = null)
  * @method Message|null findOneBy(array $criteria, array $orderBy = null)
- * @method Message[]    findAll()
  * @method Message[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class MessageRepository extends ServiceEntityRepository
@@ -21,6 +20,11 @@ class MessageRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Message::class);
+    }
+
+    public function findAll()
+    {
+        return $this->findBy(array(), array('posted' => 'DESC'));
     }
 
     /**
